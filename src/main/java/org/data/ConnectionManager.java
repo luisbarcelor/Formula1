e6c6d34c08656c785482c9e53d6e0cf90eb00a7e
luisbarcelo <luisbarcelor@hotmail.com>
2022-10-16 23:35:26 +0200

    App completed

--- src/main/java/org/data/ConnectionManager.java
+++ src/main/java/org/data/ConnectionManager.java
@@ -3,8 +3,10 @@ package org.data;
 import java.sql.*;
 
 public class ConnectionManager {
+
     public static Connection newConnection() {
         Connection connection = null;
+
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/formula1?user=luis" +
                     "&password=030305");
@@ -14,28 +16,37 @@ public class ConnectionManager {
 
         return connection;
     }
+    public static String execQuery(String query) {
 
-    public static String execQuery(String query, Connection connection) {
-        String output = null;
+        String output = "";
 
-        try (PreparedStatement stm = connection.prepareStatement(query);
+        try (Connection connection = newConnection();
+             PreparedStatement stm = connection.prepareStatement(query);
              ResultSet rs = stm.executeQuery()) {
 
             ResultSetMetaData metaData = rs.getMetaData();
-            int columnCount = metaData.getColumnCount();
 
             while (rs.next()) {
-                int index = 0;
-                while (index < columnCount) {
-                    output += rs.getString(index);
-                    index++;
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    output += "  " + rs.getString(i);
+                    if (i == metaData.getColumnCount()) {
+                        output += "\n";
+                    }
                 }
             }
 
         } catch (SQLException e) {
-            System.out.println("ERROR! VERIFICA LA SINTAXIS");
+            System.out.println("ERROR! CONSULTA INCORRECTA");
         }
 
         return output;
+    }
+    public static String obtenerEscuderia(String id) {
+        String query = "SELECT Nombre FROM Escuderia WHERE ID='" + id + "';";
+        return execQuery(query);
+    }
+    public static String obtenerNombrePilotos() {
+        String query = "SELECT Nombre from Piloto;";
+        return execQuery(query);
     }
 }
--- src/main/java/org/data/Main.java
+++ src/main/java/org/data/Main.java
@@ -5,17 +5,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String query, output;
+        String name;
+        String output;
+        String nombreEscuderia;
+        String query;
+        int option;
+
 
         System.out.println("CONSULTA BASE DE DATOS");
-        System.out.print("CONSULTA SQL: ");
-        query = scanner.nextLine();
+        System.out.println("QUE QUIERE HACER?");
+        System.out.println("1 - CONSULTA DE ESCUDERIA");
+        System.out.println("2 - CONSULTA PERSONALIZADA");
+        System.out.println("---------------------------");
+        option = Integer.parseInt(scanner.nextLine());
 
-        System.out.println();
-        System.out.println("EJECUTA \"" + query + "\"");
-        System.out.println();
-
-        output = ConnectionManager.execQuery(query, ConnectionManager.newConnection());
-        System.out.println(output);
+        switch (option) {
+            case 1 -> {
+                System.out.println("INDICA EL NOMBRE DEL PILOTO:");
+                System.out.println("----------------------------");
+                System.out.println(ConnectionManager.obtenerNombrePilotos());
+                System.out.println("----------------------------");
+                name = scanner.nextLine();
+                query = "SELECT Escuderia_ID FROM Piloto WHERE Nombre='" + name + "';";
+                output = ConnectionManager.execQuery(query);
+                nombreEscuderia = ConnectionManager.obtenerEscuderia(output);
+                System.out.println("ESCUDERIA:" + nombreEscuderia);
+            }
+            case 2 -> {
+                System.out.println("CONSULTA SQL:");
+                query = scanner.nextLine();
+                output = ConnectionManager.execQuery(query);
+                System.out.println();
+                System.out.println(output);
+            }
+            default -> System.out.println("ERROR! OPCION INCORRECTA");
+        }
     }
 }
